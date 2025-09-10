db85298fdfaa16abd11aaa7a9d40d5148b4d590a
Janco Tanis <janco.tanis@coas.com>
2025-09-10 16:38:16 +0200

    and using correct keys for test

--- test/asset_helper_test.rb
+++ test/asset_helper_test.rb
@@ -10,7 +10,7 @@ describe 'AssetHelper' do
     ## write test for AssetHelper.construct_asset
     field1 = Field.new('This is a custom Field', 'value1')
     field2 = Field.new('Another Field', 'value2')
-    asset = Asset.new({ 'id': 1, 'name': 'Asset 1', 'company_id': 101 }, [field1, field2])
+    asset = Asset.new({ 'id' => 1, 'name' => 'Asset 1', 'company_id' => 101 }, [field1, field2])
     result = Hudu::AssetHelper.construct_asset(asset)
     assert(result['name'], 'Asset 1')
     assert(result['custom_fields'].first['this_is_a_custom_field'], 'value1')
